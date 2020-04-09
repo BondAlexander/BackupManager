08c87f59c8ad5f83c27f3a1323feae9ddd01c5b0
Bond Alexander <bondalexander@Bonds-MacBook-Pro.local>
2020-04-08 19:49:18 -0600

    Added class in header file

---
+++ BackupManager.h
@@ -0,0 +1,18 @@
+#ifndef ENEMY_H_INCLUDES
+#define ENEMY_H_INCLUDES
+#include <vector>
+#include <string>
+#include <sys/stat.h>
+
+
+class MyFileObject{
+  public:
+
+  private:
+    double        time_created;
+    std::string   orig_path;
+
+}
+
+
+#endif
